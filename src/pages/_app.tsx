cd07e02b568d4a6bc4c4439e078d9ef123cff899
Min-byeonghyun <qudgus5125@naver.com>
2025-08-15 17:44:00 +0900

    Pre-fetch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,20 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
   const onClickButton = () => {
     router.push("/book/1");
   };
+  //라우터 프리페칭 
+  useEffect(() => {
+    router.prefetch('/book/1')
+  },[])
 
+  
   return (
     <>
       <header>
