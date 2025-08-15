08aa67cf76d511e8badd08d035c3ab3dbb733fba
Min-byeonghyun <qudgus5125@naver.com>
2025-08-15 17:29:37 +0900

    Link , useRouter 네비게이팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push("/book/1");
+  };
+
+  return (
+    <>
+      <header>
+        <Link href={"/"}>home</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book"}>book</Link>
+        <div>
+          <button onClick={onClickButton}>/book/1 페이지로 이동</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
