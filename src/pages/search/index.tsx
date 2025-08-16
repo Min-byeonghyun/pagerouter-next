1be0b61bf0b40d6699ebfe5a2436a19e37e14320
Min-byeonghyun <qudgus5125@naver.com>
2025-08-16 17:25:54 +0900

    page layout setting , searchbar create

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,14 @@
+import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
 
 export default function Page() {
   const router = useRouter();
-  const { id } = router.query;
-  console.log(id);
+  const { q } = router.query;
 
-  return <h1>search : {id}</h1>;
+  return <h1>search : {q}</h1>;
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
